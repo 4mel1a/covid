0a8abace9c1076e26435ee2f69e0f6d8648439b1
Amelia Siti Nur Azizah <63657315+4mel1a@users.noreply.github.com>
2020-04-25 02:27:14 +0700

    Add files via upload

---
+++ asset/footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Footer</title>
+</head>
+<body>
+	<script src="http://localhost/siperpus/asset/jquery.js"></script>
+	<script src="http://localhost/siperpus/asset/bootstrap/js/bootstrap.min.js"></script>
+</body>
+<footer class="bg-dark text-white">
+	<div class="container">
+		<div class="row pt-3">
+			<div class="col text-center">
+				<p>Copyright 2020 Amelia Siti Nur Azizah / XR7 / 04</p>
+			</div>
+		</div>
+	</div>
+</footer>
+</html>
